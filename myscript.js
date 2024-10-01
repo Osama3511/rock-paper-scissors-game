077f74482a559055b924a588efb8f3c78b96b200
mohamad osama tarakji <hmmosamu@gmail.com>
2024-10-01 18:36:19 +0300

    finished some ui just needs a lil css

--- myscript.js
+++ myscript.js
@@ -18,21 +18,12 @@ function GetComputerChoice()
 }
 
 
-// function to get user input assigned to humanchoice
+const buttonsDiv = document.querySelector("#buttons");
 
-function GetHumanChoice()
-{
-    let humanchoice = prompt("Choose wisely rock, paper or scissors!").toLowerCase();
-
-
-    if (humanchoice === "rock" || humanchoice === "paper" || humanchoice === "scissors"){
-        return humanchoice;
-    }
-    else{
-        console.log("Invalid input!");
-    }
-}
-
+buttonsDiv.addEventListener("click", (event) => {
+    let target = event.target;
+    playround(target.id)
+})
 
 
 
@@ -49,61 +40,66 @@ let computerscore = 0;
 // print the choices function
 
 function print(humanchoice, computerchoice){
-    console.log("Your choice: " + humanchoice);
-    console.log("Computer choice: " + computerchoice);
+    const choicesDiv = document.querySelector("#choices");
+
+    choicesDiv.textContent = `You chose ${humanchoice} Computer chose ${computerchoice}`;
 }
 
 
+function computerContent(computerchoice){
+    const contentDiv = document.querySelector("#content");
+    if (computerchoice === "rock"){
+        contentDiv.textContent = "rock smashes scissors you lose!";
+    }
+    else if (computerchoice === "paper"){
+        contentDiv.textContent = "paper wraps around rock you lose!";
+    }
+    else{
+        contentDiv.textContent = "scissors cuts paper you lose!";
+    }
+}
+
 // play the game!
-function playGame(){
 
 
 
-    function playround()
-    {
+function playround(humanchoice)
+{
 
-        let humanchoice = GetHumanChoice();
-        let computerchoice = GetComputerChoice();
+    let computerchoice = GetComputerChoice();
+
+    const scoreDiv = document.querySelector("#score")
+    print(humanchoice, computerchoice);
+
+    const contentDiv = document.querySelector("#content");
+    // check if humanchoice and computerchoice are equal first
+    if (humanchoice === computerchoice){
+        contentDiv.textContent = "it's a draw no one gets a point!";
+    }
+    // check all the winning conditions
+    else if (humanchoice === "rock" && computerchoice === "scissors"){
+        contentDiv.textContent = "rock smashes scissors you win!!";
+        humanscore +=1;
+    }
+    else if (humanchoice === "paper" && computerchoice === "rock"){
+        contentDiv.textContent = "paper covers rock you win!!";
+        humanscore += 1;
+    }
+    else if (humanchoice === "scissors" && computerchoice === "paper"){
+        contentDiv.textContent = "scissors cuts paper you win!!";
+        humanscore += 1
+    }
+    // all other conditions are when the human loses
+    else{
+        computerContent(computerchoice);
+        computerscore += 1
+    }
+
+    scoreDiv.textContent = `Your score: ${humanscore} Computer score: ${computerscore}`;
 
 
-
-
-        print(humanchoice, computerchoice);
-
-        // check if humanchoice and computerchoice are equal first
-        if (humanchoice === computerchoice){
-            console.log("It's a Draw!");
-        }
-
-        // check all the winning conditions
-        if (humanchoice === "rock" && computerchoice === "scissors"){
-            console.log("Rock smashes scissors you win!!");
-            humanscore +=1;
-        }
-        else if (humanchoice === "paper" && computerchoice === "rock"){
-            console.log("Paper covers rock you win!!");
-            humanscore += 1;
-        }
-        else if (humanchoice === "scissors" && computerchoice === "paper"){
-            console.log("Scissors cuts paper you win!!");
-            humanscore += 1
-        }
-        // all other conditions are when the human loses
-        else{
-            console.log("You lose!");
-            computerscore += 1
-        }
-
-        console.log(`Your score: ${humanscore}\nComputer score: ${computerscore}`);
     }
 
 
     
-    playround();
-    playround();
-    playround();
-    playround();
-    playround();
-}
 
-playGame();
